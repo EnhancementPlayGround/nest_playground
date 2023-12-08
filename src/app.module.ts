8f1e8adb43f6b64b5918226ff95943cab2ee0649
changhwan hwang <ch.hwang@ecubelabs.com>
2023-12-08 23:34:23 +0900

    refactor: remove useless files, setting.json -> settings.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './libs/health/module';
 
 @Module({
   imports: [HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
