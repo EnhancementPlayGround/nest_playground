720e74b8ba4893071b8166b37621bba2a0091a87
changhwan hwang <ch.hwang@ecubelabs.com>
2023-12-08 22:54:13 +0900

    refactor: health check lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthModule } from './health/health.module';
+import { HealthModule } from './libs/health/module';
 
 @Module({
   imports: [HealthModule],
--- src/libs/health/module.ts
+++ src/libs/health/module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
-import { HealthController } from './health.controller';
+import { HealthController } from './controller';
 
 @Module({
   imports: [TerminusModule, HttpModule],
