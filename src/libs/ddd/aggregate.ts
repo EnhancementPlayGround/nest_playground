17b1e3c22558f07f24c435339c16c13ce2def89f
changhwan hwang <ch.hwang@ecubelabs.com>
2023-12-15 16:59:42 +0900

    fix: class serializer interceptor

--- src/libs/ddd/aggregate.ts
+++ src/libs/ddd/aggregate.ts
@@ -1,9 +1,12 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export class Aggregate {
   @CreateDateColumn()
+  @Exclude()
   private createdAt!: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   private updatedAt!: Date;
 }
