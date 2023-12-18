9da724d292e1b8c630a6307b9c07a02d52387da5
changhwan hwang <ch.hwang@ecubelabs.com>
2023-12-18 09:57:46 +0900

    test: e2e test 준비 (테스트 컨테이너, health check e2e test

---
+++ test/libs/health/health.e2e-test.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { HealthModule } from '../../../src/libs/health/module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [HealthModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  test('/health (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/health')
+      .expect(200)
+      .expect({
+        status: 'ok',
+        info: { 'nestjs-docs': { status: 'up' } },
+        error: {},
+        details: { 'nestjs-docs': { status: 'up' } },
+      });
+  });
+});
