f5442e0f2b7d9a490ac0ca307433de55722ae3b0
changhwan hwang <ch.hwang@ecubelabs.com>
2023-12-20 23:10:14 +0900

    fix: remove useless binding

--- src/libs/transactional/index.ts
+++ src/libs/transactional/index.ts
@@ -5,7 +5,7 @@ export function injectTransactionalEntityManager(transactionalEntityManager: Ent
     async (
       ...args: T[M] extends (...args: infer U) => any ? U : never
     ): Promise<Awaited<T[M] extends (...args: any[]) => infer U ? U : never>> =>
-      (repository[methodName] as (...args: any[]) => any).bind(repository)({
+      (repository[methodName] as (...args: unknown[]) => any)({
         // @ts-expect-error NOTE: 현재 repository method의 파라미터가 다양하여 transactionalEntityManager를 받으려면 객체 하나만 받아야한다.
         ...args[0],
         transactionalEntityManager,
